c70dab255b2b6d53fef725cc4547c1792e28f08f
Tony Miller <anthonymiller@pursuit.org>
2019-03-15 23:55:57 -0400

    Add buttons and links to html pages

--- seed.sql
+++ seed.sql
@@ -15,7 +15,7 @@ CREATE TABLE customers (
   id SERIAL PRIMARY KEY,
   first_name VARCHAR NOT NULL,
   last_name VARCHAR NULL,
---   cust_address VARCHAR NOT NULL,
+  cust_address VARCHAR NOT NULL,
   email VARCHAR UNIQUE NOT NULL,
   cust_businesss VARCHAR NULL
 );
@@ -29,10 +29,46 @@ CREATE TABLE orders (
 INSERT INTO customers (first_name, last_name, email, city) VALUES
 ('John', 'Smith', 'johnsmith@email.com', 'East Harlem Bodega), 
 ('Michelle', 'Obama', 'michelleobama@email.com', 'Chicago Deep Dish'),
-('Guelson', NULL, 'guelson@email.com', 'Fruit stand 175th St');
+('Guelson', NULL, ' drake@email.com', 'California'),
+('Micheal B.', 'Jordan', 'michealbjordan@email.com', 'California'),
+('Micheal', 'Jordan', 'jumpman@email.com', 'Chicago'),
+('Lebron', 'James', 'lebronjames@email.com', 'Cleveland');
 
--- INSERT INTO orders (customer_id, amount) VALUES 
--- (1, 356.9),
--- (2, 87.0),
--- (3, 22.0),
+INSERT INTO orders (customer_id, amount) VALUES 
+(2, 356.9),
+(10, 87.0),
+(10, 22.0),
+(3, 6),
+(4, 30.05),
+(2, 76.20),
+(2, 35.4),
+(4, 100.00),
+(4, 200.01),
+(6, 999.99),
+(3, 666);
 
+DROP DATABASE IF EXISTS petpedia;  
+CREATE DATABASE petpedia;
+
+\c petpedia;
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR NOT NULL,
+  email VARCHAR NOT NULL
+);
+
+CREATE TABLE pets (
+  id SERIAL PRIMARY KEY,
+  owner INT REFERENCES users(id) NOT NULL,
+
+  type VARCHAR NOT NULL,
+  name VARCHAR NOT NULL,
+  age INT
+);
+
+INSERT INTO users (name, email) VALUES
+('John', 'john@email.com'), ('Michelle', 'michelle@email.com');
+
+INSERT INTO pets (owner, type, name, age) VALUES
+(2, 'bunny', 'Marshmello', 1), (2, 'dog', 'Chai', 2), (1, 'cat', 'Pixel', 5);
